e52e580516296eb86c85dbd5f82ba49536e64a96
ChaianeGerevini <chaiagerevini@hotmail.com>
2026-06-16 20:14:28 -0300

    clientes

--- src/pages/Clientes/clientes.jsx
+++ src/pages/Clientes/clientes.jsx
@@ -10,7 +10,7 @@ import {
 } from "firebase/firestore";
 
 import { db } from "../../services/firebase";
-import "./Clientes.css";
+import "./clientes.css";
 
 function Clientes() {
 const [clientes, setClientes] = useState([]);
