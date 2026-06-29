2890a52ce9257a16289b2ad3420987a69ae82370
ChaianeGerevini <chaiagerevini@hotmail.com>
2026-06-29 14:19:59 -0300

    dnv

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export default function Landing() {
+export default function RedirectLanding() {
   const { code } = useParams();
 
   useEffect(() => {
-    if (code) {
-      localStorage.setItem("referral", code);
-    }
+    // salva referral (opcional)
+    localStorage.setItem("referral", code);
 
-    window.location.replace("https://bqsh6c.mimo.run/index.html");
+    // SEMPRE manda pra landing externa
+    window.location.href =
+      `https://bqsh6c.mimo.run/index.html?ref=${code}`;
   }, [code]);
 
   return <p>Redirecionando...</p>;
