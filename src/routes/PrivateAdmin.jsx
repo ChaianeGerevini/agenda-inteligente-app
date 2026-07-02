d1cd46810847f9902d90e8c9a76bab40df9305ed
ChaianeGerevini <chaiagerevini@hotmail.com>
2026-07-02 18:09:16 -0300

    vai

--- src/routes/PrivateAdmin.jsx
+++ src/routes/PrivateAdmin.jsx
@@ -32,7 +32,7 @@ export default function PrivateAdmin({ children }) {
 
   if (loading) return <p>Carregando...</p>;
 
-  if (!isAdmin) return <Navigate to="./admin" />;
+  if (!isAdmin) return <Navigate to="./Admin" />;
 
   return children;
 }
