d36db56a375d0c3f4572ec74c7255ac710046129
ChaianeGerevini <chaiagerevini@hotmail.com>
2026-07-14 00:48:48 -0300

    importe

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { db } from "../../services/firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB5YKKotSzVORSifnobZabxscUvSpPGhUM",
